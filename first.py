db4cfc715594cfaa61d0bde7f239016314d0dbc4
JStaneyM <43329994+JStaneyM@users.noreply.github.com>
2018-09-17 11:37:17 +0530

    first 2

--- first.py
+++ first.py
@@ -1,10 +1,9 @@
 # -*- coding: utf-8 -*-
 """
 Created on Tue Sep 11 21:49:22 2018
-
 @author: Gupta2 Abhishek
 """
-#Jjjjj
+
 #importing the libraries
 import os
 import sys
@@ -15,5 +14,16 @@ print("My first python code")
 #calculating sum of two numbers
 x=2
 y=4
-z=0
+
 print("Sum of two numbers is {}".format(x+y))
+
+
+#creating pandas DataFrame
+import pandas as pd
+
+data=pd.DataFrame.from_dict(data={'A':{0:1,1:2,2:3},
+                             'B':{0:3,1:4,2:5},
+                             'C':{0:7,1:8,2:9}
+                             })
+
+data.head(1)
